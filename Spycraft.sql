d3b853a5c36ade0b9bdcb3a292a76de493b2891e
Thierry Castiglioni <castth@hotmail.com>
2022-06-23 11:57:51 +0200

    Modif specialities

--- Spycraft.sql
+++ Spycraft.sql
@@ -22,9 +22,13 @@ CREATE TABLE agents (
   birthdate DATE NOT NULL,
   code_agent INT(11) NOT NULL UNIQUE,
   nationality INT(11) NOT NULL,
-  speciality INT(11) NOT NULL,
+  speciality_one INT(11) NOT NULL,
+  speciality_two INT(11) DEFAULT NULL,
+  speciality_three INT(11) DEFAULT NULL,
   FOREIGN KEY (nationality) REFERENCES countries(id),
-  FOREIGN KEY (speciality) REFERENCES specialities(id)
+  FOREIGN KEY (speciality_one) REFERENCES specialities(id),
+  FOREIGN KEY (speciality_two) REFERENCES specialities(id),
+  FOREIGN KEY (speciality_three) REFERENCES specialities(id)
 ) engine=InnoDB;
 
 CREATE TABLE targets (
@@ -108,7 +112,7 @@ INSERT INTO countries (location) VALUES
   ('Qatar'), ('Roumanie'), ('Royaume-Uni'), ('Russie'), ('Rwanda'),
   ('Salvador'), ('Sénégal'), ('Serbie'), ('Singapour'), ('Slovaquie'), ('Suède'), ('Suisse'),
   ('Tadjikistan'), ('Tchad'), ('Thailande'), ('Tunisie'), ('Turquie'),
-  ('Ukraine'), ('Uruguay'), ('Venezuela'), ('Vietnam'), ('Yémen'), ('Zimbabwe')
+  ('Ukraine'), ('Uruguay'), ('Venezuela'), ('Vietnam'), ('Yémen'), ('Zimbabwe');
 
 
 INSERT INTO codenames (alias) VALUES
@@ -131,28 +135,53 @@ INSERT INTO specialities (skill) VALUES
   ('Désinformation'), ('Élimination'), ('Exfiltration'), ('Explosifs'), ('Filature'),
   ('Infiltration'), ('Intimidation'), ('Hacking'), ('Nettoyage'), ('Sabotage');
 
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Gilbertina', 'Palfreeman', '1975-09-10', 416, 24, 7);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Thatch', 'Corkish', '1990-05-03', 828, 3, 6);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Brendin', 'Steabler', '1985-03-18', 144, 11, 9);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Ansel', 'Bilbey', '1988-03-06', 500, 20, 3);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Ward', 'Kingdon', '1986-02-10', 659, 14, 5);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Bridgette', 'Draisey', '1972-07-27', 755, 5, 10);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Ilyse', 'Krimmer', '1995-03-02', 455, 27, 4);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Jehu', 'Danneil', '1984-02-16', 405, 11, 7);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Shep', 'Jaqueme', '1977-03-05', 940, 15, 5);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Raul', 'Greensall', '1970-07-24', 724, 19, 9);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Curry', 'Icke', '1979-07-09', 793, 30, 3);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Vern', 'Mullane', '1970-03-22', 478, 27, 3);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Ashleigh', 'Brandrick', '1982-03-31', 584, 8, 2);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Bonnee', 'Sibbet', '1977-01-24', 537, 22, 6);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Agnella', 'Bails', '1981-11-22', 840, 25, 6);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Tiffi', 'Gouldeby', '1984-07-13', 542, 19, 3);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Natalie', 'Poe', '1978-10-30', 550, 30, 2);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Woodie', 'Goodliffe', '1989-08-15', 991, 3, 3);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Rodrick', 'Popeley', '1973-12-23', 561, 9, 1);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Vinita', 'Dobrowski', '1989-02-18', 616, 18, 3);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Julieta', 'Shovelton', '1989-02-19', 221, 21, 3);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Cora', 'Pringour', '1994-11-03', 549, 13, 5);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Raimondo', 'Rohlfs', '1975-11-19', 763, 25, 3);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Cleavland', 'Wiper', '1982-07-07', 204, 12, 9);
-INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality) VALUES ('Chadwick', 'Geeraert', '1971-01-09', 944, 9, 8);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two)
+VALUES ('Gilbertina', 'Palfreeman', '1975-09-10', 416, 24, 1, 7);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two)
+VALUES ('Thatch', 'Corkish', '1990-05-03', 828, 3, 3, 6);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two)
+VALUES ('Brendin', 'Steabler', '1985-03-18', 144, 11, 2, 9);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two, speciality_three)
+VALUES ('Ansel', 'Bilbey', '1988-03-06', 500, 20, 3, 5, 8);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two)
+VALUES ('Ward', 'Kingdon', '1986-02-10', 659, 14, 5, 6);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two)
+VALUES ('Bridgette', 'Draisey', '1972-07-27', 755, 5, 8, 10);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Ilyse', 'Krimmer', '1995-03-02', 455, 27, 4);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Jehu', 'Danneil', '1984-02-16', 405, 11, 7);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Shep', 'Jaqueme', '1977-03-05', 940, 15, 5);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Raul', 'Greensall', '1970-07-24', 724, 19, 9);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Curry', 'Icke', '1979-07-09', 793, 30, 3);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two)
+VALUES ('Vern', 'Mullane', '1970-03-22', 478, 27, 3, 5);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Ashleigh', 'Brandrick', '1982-03-31', 584, 8, 2);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Bonnee', 'Sibbet', '1977-01-24', 537, 22, 6);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Agnella', 'Bails', '1981-11-22', 840, 25, 6);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Tiffi', 'Gouldeby', '1984-07-13', 542, 19, 3);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Natalie', 'Poe', '1978-10-30', 550, 30, 2);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Woodie', 'Goodliffe', '1989-08-15', 991, 3, 3);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Rodrick', 'Popeley', '1973-12-23', 561, 9, 1);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two, speciality_three)
+VALUES ('Vinita', 'Dobrowski', '1989-02-18', 616, 18, 2, 5, 10);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Julieta', 'Shovelton', '1989-02-19', 221, 21, 3);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Cora', 'Pringour', '1994-11-03', 549, 13, 5);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one)
+VALUES ('Raimondo', 'Rohlfs', '1975-11-19', 763, 25, 3);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two, speciality_three)
+VALUES ('Cleavland', 'Wiper', '1982-07-07', 204, 12, 2, 7, 9);
+INSERT INTO agents (first_name, last_name, birthdate, code_agent, nationality, speciality_one, speciality_two)
+VALUES ('Chadwick', 'Geeraert', '1971-01-09', 944, 9, 1, 8);
