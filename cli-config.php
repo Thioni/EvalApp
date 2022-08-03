ed65bd68fb1129c1551d2cc47e32dd5434874bf0
Thioni <88463304+Thioni@users.noreply.github.com>
2022-08-03 11:20:44 +0200

    Delete cli-config.php

--- cli-config.php
+++
@@ -1,9 +0,0 @@
-<?php 
-// Doctrine
-
-require_once 'vendor/autoload.php';
-require_once 'entityManager.php';
-
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
-
-return ConsoleRunner::createHelperSet(getEntityManager());
